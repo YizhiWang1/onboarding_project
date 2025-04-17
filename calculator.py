03fa78b222ad28d4fcdaf2e8f8cf4afd987f27cf
Yizhi <Yizhi@bidhaus.com>
2025-04-17 16:42:45 -0400

    Handled divide by zero in divide()

--- calculator.py
+++ calculator.py
@@ -3,6 +3,8 @@ def multiply(a: int, b: int):
     return a * b
 # division
 def divide(a: int, b: int):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
     return a / b
 if __name__ == "__main__":
     pass
